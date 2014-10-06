c51d7492382f8f2a1d5899ab3ed62b6ef49468dd
Jon Bauman <jongithub@shumi.fake>
2014-10-05 23:16:00 -0700

    Create new ccsubs rails app
    Command used: rails new ccsubs -T -d postgresql

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ccsubs::Application.config.secret_key_base = '0c2193f506cbf3ce9f677733a5925ea04d48b3364c0356de21fa4bf80878c17b4634ff86ab47a45e0da4f4072847edb70e919ee011a32ac4ddd7c2d481bbb3c8'
