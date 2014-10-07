9fd61e823aa64d9d27ba9db25cc13c2e490ef754
Jon Bauman <jongithub@shumi.fake>
2014-10-07 00:05:02 -0700

    Add some User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
   has_many :requests
+  validates :name, presence: true, uniqueness: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@([a-z\d\-]+\.)+[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
 end
