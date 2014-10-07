c6700affac8d16524d9b077df5764c9c8d046912
Jon Bauman <jongithub@shumi.fake>
2014-10-06 23:00:00 -0700

    Add users controller
    Command: rails generate controller Users new -T

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Ccsubs::Application.routes.draw do
+  get "users/new"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
