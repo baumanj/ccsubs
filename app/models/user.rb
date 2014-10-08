bc966b29ac96f4618b553e16c3f49a2fc3115f73
Jon Bauman <jongithub@shumi.fake>
2014-10-07 23:49:05 -0700

    Downcase the email before save to ensure index uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@([a-z\d\-]+\.)+[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
+  before_save do
+    self.email.downcase!
+  end
 end
