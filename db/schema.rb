39ce8a4f36f6b7e712e5db0756c03734ee10c080
Jon Bauman <jongithub@shumi.fake>
2014-10-07 23:32:48 -0700

    Add index to email column of Users table
    Commands:
    rails generate migration add_index_to_users_email
    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141007064319) do
+ActiveRecord::Schema.define(version: 20141008063122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,5 +31,7 @@ ActiveRecord::Schema.define(version: 20141007064319) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
