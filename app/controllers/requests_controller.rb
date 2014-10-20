096e8e4ca9b18704cc498d6a4a30960db54c3ea4
Jon Bauman <jongithub@shumi.fake>
2014-10-19 17:16:54 -0700

    Add support for editing requests

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -8,11 +8,10 @@ class RequestsController < ApplicationController
 
   def create
     @request = Request.new(request_params)
-    @request.start = DateTime.parse(params[:request][:start])
     @request.user = current_user
     if @request.save
       flash[:success] = "Request created"
-      redirect_to @request
+      redirect_to requests_path
     else
       @errors = @request.errors
       render 'new' # Try again
@@ -20,21 +19,17 @@ class RequestsController < ApplicationController
   end
 
   def edit
-    # @user = User.find(params[:id])
+    @request = Request.find(params[:id])
   end
 
   def update
-    # @user = User.find(params[:id])
-    # if params[:user][:password].empty? &&  params[:user][:password_confirmation].empty?
-    #   params[:user].delete(:password)
-    #   params[:user].delete(:password_confirmation)
-    # end
-    # if @user.update_attributes(user_params)
-    #   flash[:success] = "Update successful"
-    #   redirect_to @user
-    # else
-    #   render 'edit' # Try again
-    # end
+    @request = Request.find(params[:id])
+    if @request.update_attributes(request_params)
+      flash[:success] = "Update successful"
+      redirect_to requests_path
+    else
+      render 'edit' # Try again
+    end
   end
 
   def index
@@ -48,6 +43,6 @@ class RequestsController < ApplicationController
   private
 
     def request_params
-      params.require(:request).permit(:start, :text)
+      params.require(:request).permit(:start, :shift, :text)
     end
 end
--- app/models/request.rb
+++ app/models/request.rb
@@ -3,4 +3,15 @@ class Request < ActiveRecord::Base
   validates :start, presence: true
   validates :user, presence: true
   # TODO: Validate start/end time
+
+  SHIFTS = { '8-12:30' => 8, '12:30-5' => 12, '5-9' => 17, '9-1' => 21 }
+
+  def shift
+    start.hour if start
+  end
+
+  def shift=(val)
+    self.start = start - start.hour.hours + val.to_i.hours
+  end
+
 end
