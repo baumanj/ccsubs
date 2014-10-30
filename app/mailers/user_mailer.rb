9bc4714aa20ed699f837ab5d12ce604f928c315c
Jon Bauman <jongithub@shumi.fake>
2014-10-29 18:08:50 -0700

    Generate mailers
    Command: rails generate mailer user_mailer confirm_email reset_password

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,25 @@
+class UserMailer < ActionMailer::Base
+  default from: "from@example.com"
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.confirm_email.subject
+  #
+  def confirm_email
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.user_mailer.reset_password.subject
+  #
+  def reset_password
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
