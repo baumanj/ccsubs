621ba1b22498f9a9c86b90786be0d8dafb75b28d
Jon Bauman <jongithub@shumi.fake>
2014-11-05 22:00:47 -0800

    Add production email configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,6 @@ Ccsubs::Application.configure do
   config.action_controller.perform_caching = false
 
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :test
   host = 'localhost:3000'
   config.action_mailer.default_url_options = { host: host }
   config.action_mailer.delivery_method = :smtp
