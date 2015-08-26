4c778facb2ecea9931934abaaffb7ee6dd47e373
Jon Bauman <jongithub@shumi.fake>
2015-08-26 02:15:50 -0700

    Initial email notification of added matching availability

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -11,8 +11,16 @@ class Availability < ActiveRecord::Base
   validate :shift_is_in_the_future
   validate :no_schedule_conflicts
 
-  def matching_requests
-    Request.where(fulfilled: false).where(start: self.start).order(:id)
+  after_create do
+    availability_user_open_requests = user.open_requests
+    if availability_user_open_requests.any?
+      Request.seeking_offers.where(date: self.date, shift: self.shift_to_i).each do |req|
+        UserMailer.notify_matching_avilability(req, availability_user_open_requests).deliver
+      end
+    end
+    # find the users with outstanding requests matching this and notify them
+    # v2: only send a notification email if there have been new matching availabilities
+    # added since the last time the user visited the site (or maybe 1/day)
   end
 
   def shift_is_in_the_future
