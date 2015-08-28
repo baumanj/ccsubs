8ec230d0e9bb11e4a583de7f2eab2cdd48af39ba
Jon Bauman <jongithub@shumi.fake>
2015-08-28 11:42:04 -0700

    Remove availability nofitication email code; it's not ready for production yet.

--- app/models/availability.rb
+++ app/models/availability.rb
@@ -10,18 +10,6 @@ class Availability < ActiveRecord::Base
   validates :user, presence: true
   validates_with ShiftTimeValidator
 
-  after_create do
-    availability_user_open_requests = user.open_requests
-    if availability_user_open_requests.any?
-      Request.seeking_offers.where(date: self.date, shift: self.shift_to_i).each do |req|
-        UserMailer.notify_matching_avilability(req, availability_user_open_requests).deliver
-      end
-    end
-    # find the users with outstanding requests matching this and notify them
-    # v2: only send a notification email if there have been new matching availabilities
-    # added since the last time the user visited the site (or maybe 1/day)
-  end
-
   def tentative?
     request && !request.fulfilled?
   end
