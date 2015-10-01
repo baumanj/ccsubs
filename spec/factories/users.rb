8f813a38ad5b19a1cad09d56d39608b7633e67a4
Jon Bauman <jongithub@shumi.fake>
2015-09-30 17:41:26 -0700

    Add Faker::Number.unique_number so we have unique vics

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,24 @@
+module Faker
+  class Number
+    def self.unique_number(digits)
+      @previously_selected ||= []
+      raise if @previously_selected.size >= 10**digits
+      loop do
+        n = number(digits)
+        unless @previously_selected.include?(n)
+          @previously_selected << n
+          return n
+        end
+      end
+    end
+  end
+end
+
 FactoryGirl.define do
   factory :user do
     name { Faker::Name.name }
     email { Faker::Internet.email }
-    vic { Faker::Number.number(5) }
+    vic { Faker::Number.unique_number(5) }
     password Faker::Internet.password(min_length = 5)
   end
 end
