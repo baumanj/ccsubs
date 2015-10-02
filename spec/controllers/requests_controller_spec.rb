ebf5bedeb1ff861cfe9d7c3193e9308d4f0870d8
Jon Bauman <jongithub@shumi.fake>
2015-10-01 22:02:56 -0700

    More DRY and add some more RequestController tests

--- spec/controllers/requests_controller_spec.rb
+++ spec/controllers/requests_controller_spec.rb
@@ -1,41 +1,51 @@
 require 'controllers/shared'
 
+shared_context "let request = @request", assigns: :request do
+  let(:request) { assigns(:request) }
+end
 
 describe RequestsController do
 
-  ['index', 'fulfilled', 'owned_index'].each do |action|
-    describe "GET '#{action}'" do
-      it_behaves_like "an action needing login"
+  describe "GET 'new'", requires: :confirmed_current_user, assigns: :request do
+    context "when logged in and confirmed" do
+      include_context "logged in and confirmed"
+      before { get 'new' }
 
-      context "when logged in but unconfirmed" do
-        include_context "logged in"
-        it_behaves_like "an action needing user confirmation"
-      end
-
-      context "when logged in and confirmed" do
-        include_context "logged in and confirmed"
-
-        it "returns http success" do
-          get action
-          expect(response).to be_success
-        end
+      it "assigns @request to a new Request for the current user" do
+        expect(request).to be_a_new(Request)
+        expect(request.user).to eq(subject.current_user)
       end
     end
   end
 
-  describe "#new" do it end
-  describe "#create" do it end
+  describe "POST 'create'", requires: :confirmed_current_user, assigns: :request do
+    context "when logged in and confirmed" do
+      include_context "logged in and confirmed"
+
+      it "saves a new Request for current_user" do
+        post 'create', request: attributes_for(:request)
+        expect(request).to be_persisted
+        expect(request.user).to eq(subject.current_user)
+        # byebug
+        expect(response).to redirect_to(request)
+      end
+    end
+  end
+
   describe "#show" do it end
   describe "#old_create" do it end
   describe "#update" do it end
   describe "#offer_sub" do it end
 
-  describe "#index" do
-    include_context "logged in and confirmed"
-    before { get 'index' }
+  describe "GET 'index'", requires: :confirmed_current_user do
+    context "when logged in and confirmed" do
+      include_context "logged in and confirmed"
+      before { get 'index' }
 
-    it "shows active requests" do
-      expect(assigns(:requests)).to eq(Request.active)
+      it "shows active requests" do
+        expect(response).to be_success
+        expect(assigns(:requests)).to eq(Request.active)
+      end
     end
   end
 
--- spec/controllers/shared.rb
+++ spec/controllers/shared.rb
@@ -6,27 +6,33 @@ shared_context "logged in and confirmed" do
   before { subject.current_user = create(:confirmed_user) }
 end
 
+shared_context "current user must be confirmed", requires: :confirmed_current_user do
+  it_behaves_like "an action needing login"
+
+  context "when logged in but unconfirmed" do
+    it_behaves_like "an action needing user confirmation"
+  end
+end
+
 # Expects the first example group under the "describe controller" group to be
 # a string of the form "HTTP_METHOD 'controller_action'"
 # e.g., "GET 'index'" or "PATCH 'offer_sub'"
-redirect_shared_example = proc do |context: nil, expect_redirect_to: :url_for, expect_redirect_to_url_for: nil|
+example_proc = proc do |context: nil, expect_redirect_to: nil|
   proc do
     include_context context unless context.nil?
     controller, method, action = metadata[:full_description].scan(/\w+/)
-    it "redirects to #{expect_redirect_to} #{Array(expect_redirect_to_url_for).join(', ')}" do
-      resolved_expect_redirect_to_url_for = *Array(expect_redirect_to_url_for).map {|a| subject.send(a) }
-      redirect_url = subject.send(expect_redirect_to, resolved_expect_redirect_to_url_for)
+    it "redirects to #{expect_redirect_to}" do
       send(method.downcase, action)
-      expect(response).to redirect_to(redirect_url)
+      expect(response).to redirect_to(subject.send(expect_redirect_to))
     end
   end
 end
 
 shared_examples("an action needing login",
-  &redirect_shared_example.call(expect_redirect_to: :signin_url))
+  &example_proc.call(expect_redirect_to: :signin_url))
 
 shared_examples("an action needing user confirmation",
-  &redirect_shared_example.call(context: "logged in", expect_redirect_to_url_for: :current_user))
+  &example_proc.call(context: "logged in", expect_redirect_to: :current_user))
 
 shared_examples("an action needing admin",
-  &redirect_shared_example.call(context: "logged in", expect_redirect_to: :root_url))
+  &example_proc.call(context: "logged in", expect_redirect_to: :root_url))
