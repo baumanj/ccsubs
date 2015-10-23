5f90d07c1ade2bdc1c90229ca149cc913718c497
Jon Bauman <jongithub@shumi.fake>
2015-10-23 15:13:26 -0700

    Add support for creating FactoryGirl requests in the past

--- spec/factories/requests.rb
+++ spec/factories/requests.rb
@@ -1,29 +1,43 @@
 module Faker
   class Date
-    def self.unique_in_the_next_year
+    def self.unique(date_method)
       @previously_selected ||= []
-      @previously_selected << in_the_next_year(excluding: @previously_selected)
+      @previously_selected << send(date_method, excluding: @previously_selected)
       @previously_selected.last
     end
 
     def self.in_the_next_year(excluding: nil)
-      dates_in_next_year = ((1.day.from_now.to_date)...(1.year.from_now.to_date)).to_a
-      (dates_in_next_year - [*excluding]).sample
+     in_date_range((1.day.from_now.to_date)...(1.year.from_now.to_date), excluding: excluding)
+    end
+
+    def self.in_the_past_year(excluding: nil)
+      in_date_range((1.year.ago.to_date)...(1.day.ago.to_date), excluding: excluding)
+    end
+
+    def self.in_date_range(range, excluding: nil)
+      (range.to_a - [*excluding]).sample
     end
   end
+end
+
+date_in_the_past_year = proc do
+  to_create {|instance| instance.save(validate: false) }
+  date { Faker::Date.unique(:in_the_past_year) }
 end
 
 FactoryGirl.define do
   factory :request, aliases: [:seeking_offers_request] do
     user
-    date { Faker::Date.unique_in_the_next_year }
+    date { Faker::Date.unique(:in_the_next_year) }
     shift { Request.shifts.keys.sample }
+
+    factory :past_request, aliases: [:past_seeking_offers_request], &date_in_the_past_year
 
     factory :sent_offer_request, class: Request do
       transient do
-        sent_offer_request_date { Faker::Date.unique_in_the_next_year }
+        sent_offer_request_date { Faker::Date.unique(:in_the_next_year) }
         sent_offer_request_shift { Request.shifts.keys.sample }
-        received_offer_request_date { Faker::Date.unique_in_the_next_year }
+        received_offer_request_date { Faker::Date.unique(:in_the_next_year) }
         received_offer_request_shift { Request.shifts.keys.sample }
       end
 
@@ -41,10 +55,14 @@ FactoryGirl.define do
       end
       after(:build) {|r| r.fulfilling_swap.fulfilling_swap = r }
 
+      factory :past_sent_offer_request, &date_in_the_past_year
+
       factory :received_offer_request do
         after(:build) do |r|
           r.state, r.fulfilling_swap.state = r.fulfilling_swap.state, r.state
         end
+
+        factory :past_received_offer_request, &date_in_the_past_year
       end
 
       factory :fulfilled_request do
@@ -54,6 +72,8 @@ FactoryGirl.define do
             s.availability.free = false
           end
         end
+
+        factory :past_fulfilled_request, &date_in_the_past_year
       end
     end
   end
