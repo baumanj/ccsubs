b76a6e579533ff0efa550a8cb8cf24c17ab21365
Jon Bauman <baumanj@users.noreply.github.com>
2018-06-23 12:17:40 -0400

    More shift time utility tests

--- spec/models/shift_time_spec.rb
+++ spec/models/shift_time_spec.rb
@@ -1,19 +1,23 @@
 describe ShiftTime do
 
-  it "returns the right next shift start" do
+  dates = [
+    { year: 2018, month:  3, day: 10 },
+    { year: 2018, month:  3, day: 11 }, # DST begin
+    { year: 2018, month:  3, day: 12 },
+    { year: 2018, month:  6, day: 20 },
+    { year: 2018, month: 11, day:  3 },
+    { year: 2018, month: 11, day:  4 }, # DST end
+    { year: 2018, month: 11, day:  5 },
+  ].map {|d| Date.new(d[:year], d[:month], d[:day])}
 
-    dates = [
-      { year: 2018, month:  3, day: 10 },
-      { year: 2018, month:  3, day: 11 }, # DST begin
-      { year: 2018, month:  3, day: 12 },
-      { year: 2018, month:  6, day: 20 },
-      { year: 2018, month: 11, day:  3 },
-      { year: 2018, month: 11, day:  4 }, # DST end
-      { year: 2018, month: 11, day:  5 },
-    ]
+  dates.each do |date|
+    it "starts the first shift at 8am on #{date}" do
+      expect(ShiftTime.first_shift_start(date).hour).to eq(8)
+    end
+  end
 
-    dates.each do |date|
-      date = Date.new(date[:year], date[:month], date[:day])
+  dates.each do |date|
+    it "returns the right next shift start for #{date}" do
 
     	expect(ShiftTime.next_shift_start(date.in_time_zone.change(hour:  0, min: 59, sec: 59))).to \
     		                             eq(date.in_time_zone.change(hour:  8, min:  0, sec:  0))
